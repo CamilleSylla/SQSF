63eddf3866e6b2f92f81a75c48de17f8464315a2
Camille Sylla <camillesylla.web@gmail.com>
2021-03-11 19:29:22 +0100

    added search on catalogue page

--- pages/dashboard/produits/[seller].js
+++ pages/dashboard/produits/[seller].js
@@ -8,26 +8,35 @@ import Link from "next/link";
 import Image from "next/image";
 export default function ProduitsVendeurProfil({ profil_item }) {
   const [hide, setHide] = useState(true);
+  const [search, setSearch] = useState("");
 
   function format(n) {
-    return (n < 10 ? '0' : '') + n;
+    return (n < 10 ? "0" : "") + n;
   }
 
-  function getFommattedDate (dateF) {
-    const date = new Date(dateF)
+  function getFommattedDate(dateF) {
+    const date = new Date(dateF);
     var month = format(date.getMonth() + 1);
     var day = format(date.getDate());
     var year = format(date.getFullYear());
     return day + "/" + month + "/" + year;
   }
 
+  const Search = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <>
       <div className={style.dashboard_wrapper}>
         <DashboardMenu />
         <div className={style.el_wrapper}>
           <div className={component.filter_and_addProduct}>
-            <input className={component.search} type="text" placeholder="Recherche de produit"/>
+            <input
+              className={component.search}
+              type="text"
+              placeholder="Recherche de produit"
+              onChange={Search}
+            />
             <div className={component.filter}></div>
             <div
               className={component.addProduct}
@@ -37,38 +46,64 @@ export default function ProduitsVendeurProfil({ profil_item }) {
             </div>
           </div>
           <div className={component.addFrom}>
-            
             {hide ? null : <CreateArticle />}
           </div>
 
           <div className={component.product_card_grid}>
-            {profil_item.map((item) => {
-              return (
-                <div className={component.product_card}>
-                  <div className={component.image}>
-                    <Image
-                      src={item.images[0] ? item.images[0] : "/image.svg"}
-                      alt="Image principal"
-                      layout="fill"
-                      sizes="100%"
-                      objectFit="cover"
-                    />
+            {profil_item
+              .filter((filtered) => {
+                if (search == "") {
+                  return filtered;
+                } else if (
+                  filtered.name
+                    .toLowerCase()
+                    .includes(search.toLocaleLowerCase())
+                    ||
+                    filtered.brand
+                    .toLowerCase()
+                    .includes(search.toLocaleLowerCase())
+
+                ) {
+                  return filtered;
+                }
+              })
+              .map((item) => {
+                return (
+                  <div className={component.product_card}>
+                    <div className={component.image}>
+                      <Image
+                        src={item.images[0] ? item.images[0] : "/image.svg"}
+                        alt="Image principal"
+                        layout="fill"
+                        sizes="100%"
+                        objectFit="cover"
+                      />
+                    </div>
+                    <div className={component.rows_content}>
+                      <p>{item.name}</p>
+                      <p>{item.brand}</p>
+                      <p>{item.price}€</p>
+                      <p>Crée le {getFommattedDate(item.date)}</p>
+                    </div>
+                    <div className={component.card_menu}>
+                      <Link href={`/dashboard/produits/modify/${item._id}`}>
+                        <p
+                          className={component.card_menu_item}
+                          style={{ background: "orange" }}
+                        >
+                          Modifier
+                        </p>
+                      </Link>
+                      <button
+                        className={component.card_menu_item}
+                        style={{ background: "red" }}
+                      >
+                        Supprimer
+                      </button>
+                    </div>
                   </div>
-                  <div className={component.rows_content}>
-                    <p>{item.name}</p>
-                    <p>{item.brand}</p>
-                    <p>{item.price}€</p>
-                    <p>Crée le  {getFommattedDate(item.date)}</p>
-                  </div>
-                <div className={component.card_menu}>
-                  <Link  href={`/dashboard/produits/modify/${item._id}`}>
-                    <p className={component.card_menu_item} style={{background: "orange"}}>Modifier</p>
-                  </Link>
-                  <button className={component.card_menu_item} style={{background: "red"}}>Supprimer</button>
-                </div>
-                </div>
-              );
-            })}
+                );
+              })}
           </div>
 
           {/* <table className={component.table_wrapper}>
--- pages/market.js
+++ pages/market.js
@@ -1,37 +1,63 @@
 import axios from "axios";
 import Head from "next/head";
+import { useState } from "react";
 import CardDisplayer from "../components/CardDisplayer";
-import style from '../styles/market.module.scss'
+import style from "../styles/market.module.scss";
 
-export default function Market ({items}) {
-  
-    return <>
-    <Head>
+export default function Market({ items }) {
+  const [search, setSearch] = useState("");
+  const [toogleFilter, setToogleFilter] = useState(false);
+  return (
+    <>
+      <Head>
         <title>SQMarket - Catalogue</title>
-    </Head>
-    <div className={style.market_container}>
+      </Head>
+      <div className={style.market_container}>
         <section className={style.pages_header}>
-            <article>
-                <h1>Catalogue</h1>
-            </article>
+          <article>
+            <h1>Catalogue</h1>
+          </article>
         </section>
-        <CardDisplayer data={items}/>
-    </div>
+        <div className={style.filter_and_search}>
+          <input type="text" onChange={(e) => setSearch(e.target.value)} />
+            <div className={style.display_filter} onClick={() => setToogleFilter(!toogleFilter)}>{toogleFilter == true ? "Annuler" :  "+ Afficher les filtres"}</div>
+        </div>
+        <CardDisplayer
+          data={items.filter((filtered) => {
+            if (search == "") {
+              return filtered;
+            } else if (
+              filtered.name
+                .toLowerCase()
+                .includes(search.toLocaleLowerCase()) ||
+              filtered.brand
+                .toLowerCase()
+                .includes(search.toLocaleLowerCase()) ||
+              filtered.vendeur
+                .toLowerCase()
+                .includes(search.toLocaleLowerCase())
+            ) {
+              return filtered;
+            }
+          })}
+        />
+      </div>
     </>
+  );
 }
 
 export async function getServerSideProps() {
-    const items = await axios
-      .get("http://localhost:3001/api/inventary/all")
-      .then((res) => {
-        return res.data;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    return {
-      props: {
-        items,
-      },
-    };
-  }
+  const items = await axios
+    .get("http://localhost:3001/api/inventary/all")
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return {
+    props: {
+      items,
+    },
+  };
+}
