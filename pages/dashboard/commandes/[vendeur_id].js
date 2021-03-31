6e050f5b7b9cc6be199675e0b46d2364e260d257
CamilleSylla <camillesylla.web@gmail.com>
2021-03-31 14:22:44 +0200

    almost finish dashboard command layout

--- pages/dashboard/commandes/[vendeur_id].js
+++ pages/dashboard/commandes/[vendeur_id].js
@@ -1,8 +1,16 @@
 import axios from "axios";
 import DashboardMenu from "../../../components/DashboardMenu";
 import style from "../../../styles/maindashboard.module.scss";
+import Image from "next/image";
 
 export default function Commande({ commandes }) {
+  const orderMonth = commandes.map(commande => {
+    const currentDate = new Date ()
+    const currentMonth = currentDate.getMonth()
+    const currentYear = currentDate.getFullYear()
+    const orderDate = new Date(commande.date)
+    if (orderDate.getMonth() == currentMonth && orderDate.getFullYear() == currentYear) return commande
+  })
   if (!commandes) {
     return (
       <>
@@ -13,50 +21,129 @@ export default function Commande({ commandes }) {
     return (
       <div className={style.dashboard_wrapper}>
         <DashboardMenu />
-        <div className="main_list_container">
-          <table className="list">
-            <thead>
-              <tr>
-                <th>
-                  <p>Identifiant</p>
-                </th>
-                <th>
-                  <p>Montant</p>
-                </th>
-                <th>
-                  <p>Date</p>
-                </th>
-                <th>
-                  <p>Status</p>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {commandes.map((commande, i) => {
-                const d = new Date(commande.date);
-                return (
-                  <tr key={i}>
-                    <td>
-                      <p>id: {commande._id}</p>
-                    </td>
-                    <td>
-                      <p>{commande.amount}€</p>
-                    </td>
-                    <td>
-                      <p>
-                        {d.toLocaleDateString() +
-                          " à " +
-                          d.toLocaleTimeString()}
-                      </p>
-                    </td>
-                    <td>
-                      <input type="checkbox" />
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+
+        <div className={style.el_wrapper}>
+          <div className="dashboard_top_cards_container">
+            <div className="top_cards">
+            <div className="nav_button">
+            <Image
+                      src="/cart.svg"
+                      alt="Recherche"
+                      width="20px"
+                      height="20px"
+                      objectFit="contain"
+                    />
+                  </div>
+                  <h6 style={{width: "50%"}}>Nombre de commandes ce mois-ci :<br/><span style={{fontSize: "20px"}}>{orderMonth.length}</span></h6>
+           
+             </div>
+            <div className="top_cards">
+            <div className="nav_button">
+            <Image
+                      src="/truck.svg"
+                      alt="Recherche"
+                      width="20px"
+                      height="20px"
+                      objectFit="contain"
+                    />
+                  </div>
+                  <h6 style={{width: "50%"}}>Commandes sans notification d'envoie :<br/><span style={{fontSize: "20px"}}>{orderMonth.length}</span></h6>
+           
+            </div>
+            <div className="top_cards">
+            <div className="nav_button">
+            <Image
+                      src="/shop.svg"
+                      alt="Recherche"
+                      width="20px"
+                      height="20px"
+                      objectFit="contain"
+                    />
+                  </div>
+                  <h6 style={{width: "50%"}}>Commande Click&Collect sans récupération :<br/><span style={{fontSize: "20px"}}>{orderMonth.length}</span></h6>
+           
+            </div>
+          </div>
+          <div className={style.content_container}>
+            <table className={style.list}>
+              <thead>
+                <tr>
+                  <th>
+                    <p>Identifiant</p>
+                  </th>
+                  <th>
+                    <p>Montant</p>
+                  </th>
+                  <th>
+                    <p>Date</p>
+                  </th>
+                  <th>
+                    <p>Status</p>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {commandes.map((commande, i) => {
+                  const d = new Date(commande.date);
+                  return (
+                    <tr key={i}>
+                      <td>
+                        <p>id: {commande._id}</p>
+                      </td>
+                      <td>
+                        <p>{commande.amount}€</p>
+                      </td>
+                      <td>
+                        <p>
+                          {d.toLocaleDateString() +
+                            " à " +
+                            d.toLocaleTimeString()}
+                        </p>
+                      </td>
+                      <td>
+                        <input type="checkbox" />
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            <div className={style.card_info_container}>
+              <div className={style.card}>
+                <div className={style.user}>
+                  <div className="nav_button">
+                    <Image
+                      src="/user.svg"
+                      alt="Recherche"
+                      width="20px"
+                      height="20px"
+                      objectFit="contain"
+                    />
+                  </div>
+                  <h6 style={{marginTop: "10px"}}>Nom de l'utilisateur</h6>
+                </div>
+                <p>Telephone</p>
+                <p>Email</p>
+              </div>
+              <div className={style.card}>
+              <div className={style.user}>
+                  <div className="nav_button">
+                    <Image
+                      src="/box.svg"
+                      alt="Recherche"
+                      width="20px"
+                      height="20px"
+                      objectFit="contain"
+                    />
+                  </div>
+                  <h6 style={{marginTop: "10px"}}>Details de livraison</h6>
+                </div>
+                <p>Adresse</p>
+                <p>Email</p>
+                <p>Email</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
